6688659ffe120bb5e49eac0c8a3d4ad20b7dc728
Kanishk Yadav <ykanishk479@gmail.com>
2026-03-12 01:57:24 +0530

    fix: buysection correct links

--- frontend/src/components/BuySection.tsx
+++ frontend/src/components/BuySection.tsx
@@ -15,25 +15,25 @@ const fallbackProducts = [
     images: [{ imageUrl: "https://img.cah.io/images/vc07edlh/production/69d14a8c4c8084841b5f3437eb8a06124162dc0d-660x1270.png" }, { imageUrl: "https://img.cah.io/images/vc07edlh/production/63e9bcc5935e9cae00a4a9594d3637d89608c443-660x1270.png" }],
     tagline: "America's #1 gerbil coffin.",
     buttonText: "Buy Now",
-    buttonLink: "/products/cards-against-humanity",
+    buttonLink: "/products/more-cah",
   },
   {
     images: [{ imageUrl: "https://img.cah.io/images/vc07edlh/production/048109f3bcd6e2c21cb041f9e5d0ddeac9c3de2f-716x1294.png" }],
     tagline: "Play CAH with your kids.",
     buttonText: "Buy Family Edition",
-    buttonLink: "/products/family-edition",
+    buttonLink: "#",
   },
   {
     images: [{ imageUrl: "https://img.cah.io/images/vc07edlh/production/6122ebf50190e25b00cbfd9d7960671bf6a0c054-660x1200.png" }, { imageUrl: "https://img.cah.io/images/vc07edlh/production/e92ecce7e13c7339aa9bb54f7909e1cd9f7a8cd2-660x1200.png" }],
     tagline: "Moooooore cards!",
     buttonText: "Buy Expansions",
-    buttonLink: "/shop/expansions",
+    buttonLink: "#",
   },
   {
     images: [{ imageUrl: "https://img.cah.io/images/vc07edlh/production/41556c5c773ab42a27824ae1c8c73315653de2bf-660x1200.png" }, { imageUrl: "https://img.cah.io/images/vc07edlh/production/83bdf2fb8ba74ceca463163373d12d9ff432230b-660x1200.png" }],
     tagline: "For whatever you're into.",
     buttonText: "Buy $5 Packs",
-    buttonLink: "/shop/packs",
+    buttonLink: "#",
   },
 ];
 
